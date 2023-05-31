b7e2fda25057057645ebc069139aa03afd66bfd7
Stasai <nathanbobocea@gmail.com>
2023-05-30 17:28:43 -0700

    image update

--- js/filter-tags.js
+++ js/filter-tags.js
@@ -1,7 +1,7 @@
 (function() {
 
   var $imgs = $('#gallery img');                  // Store all images
-  var $buttons = $('#buttons');                   // Store buttons element
+  var $buttons = $('#buttons2');                   // Store buttons element
   var tagged = {};                                // Create tagged object
 
   $imgs.each(function() {                         // Loop through images and
